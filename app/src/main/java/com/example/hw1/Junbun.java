314a7e3fdd3b7ff2325ea560c40d5ec83beefc19
Hyunseo Kang <jelly9999@g.skku.edu>
2023-06-30 19:36:35 +0900

    junbun done

---
+++ app/src/main/java/com/example/hw1/Junbun.java
@@ -0,0 +1,27 @@
+package com.example.hw1;
+
+public class Junbun {
+    public String name;
+    public String number;
+    public String address;
+
+    public String getName(){
+        return name;
+    }
+    public String getNumber(){
+        return number;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setNumber(String number){
+        this.number = number;
+    }
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+}
--- app/src/main/java/com/example/hw1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hw1/ui/home/HomeFragment.java
@@ -1,37 +1,132 @@
 package com.example.hw1.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.hw1.databinding.FragmentHomeBinding;
+import com.example.hw1.Junbun;
+import com.example.hw1.ListViewAdapter;
+import com.example.hw1.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private ListView listView;
+    private ListViewAdapter listViewAdapter;
+    private ArrayList<Junbun> items;
+
+    SearchView searchView;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        listView = root.findViewById(R.id.listView);
+        searchView = root.findViewById(R.id.searchView);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        items = new ArrayList<>();
+
+        final ArrayList<Junbun> filteredItems = new ArrayList<>(items);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(), items.get(i).address, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        listViewAdapter = new ListViewAdapter(filteredItems, requireContext());
+        listView.setAdapter(listViewAdapter);
+
+        String json = "";
+
+        try {
+            InputStream inputStream = requireContext().getAssets().open("junbun.json");
+            int fileSize = inputStream.available();
+
+            byte[] buffer = new byte[fileSize];
+            inputStream.read(buffer);
+            inputStream.close();
+
+            json = new String(buffer, "UTF-8");
+            Log.d("json", json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                Junbun junbun = new Junbun();
+                junbun.setName(jsonObject.getString("name"));
+                junbun.setNumber(jsonObject.getString("number"));
+                junbun.setAddress(jsonObject.getString("address"));
+
+                items.add(junbun);
+
+            }
+
+            filterItems("", filteredItems);
+            listViewAdapter.notifyDataSetChanged();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                filterItems(query, filteredItems);
+                listViewAdapter.notifyDataSetChanged();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                filterItems(s, filteredItems);
+                listViewAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void filterItems(String query, ArrayList<Junbun> filteredItems) {
+        filteredItems.clear();
+
+        if (query.isEmpty()) {
+            filteredItems.addAll(items);
+        } else {
+            String lowerCaseQuery = query.toLowerCase();
+
+            for (Junbun junbun : items) {
+                if (junbun.getName().toLowerCase().contains(lowerCaseQuery)) {
+                    filteredItems.add(junbun);
+                }
+            }
+        }
     }
+
 }

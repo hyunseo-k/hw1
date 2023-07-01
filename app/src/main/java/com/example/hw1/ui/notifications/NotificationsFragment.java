0089ba01ed274933c6aafd2c5e325666a5976ea1
Jinhyeon Kwon <121283722+jinhyeonkwon@users.noreply.github.com>
2023-07-01 17:45:57 +0900

    20230701 1745

--- app/src/main/java/com/example/hw1/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/hw1/ui/notifications/NotificationsFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hw1.R;
 import com.example.hw1.databinding.FragmentNotificationsBinding;
@@ -20,6 +23,15 @@ public class NotificationsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+
+        TextView cocktailTitle = (TextView) root.findViewById(R.id.cocktailTitle);
+        ImageView cocktailImage = (ImageView) root.findViewById(R.id.cocktailImage);
+        TextView cocktailMethod = (TextView) root.findViewById(R.id.cocktailMethod);
+        RecyclerView cocktailIngreds = (RecyclerView) root.findViewById(R.id.cocktailIngreds);
+        EditText cocktailSearch = (EditText) root.findViewById(R.id.cocktailSearch);
+        Button buttonSearch = (Button) root.findViewById(R.id.buttonSearch);
+        Button buttonRandom = (Button) root.findViewById(R.id.buttonRandom);
+
 
         return root;
     }

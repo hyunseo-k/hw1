12d877a4608d331d3c8201987c83404afd3a2fb1
Hyunseo Kang <jelly9999@g.skku.edu>
2023-07-01 18:03:52 +0900

    cocktail class 만듦

---
+++ app/src/main/java/com/example/hw1/Cocktail.java
@@ -0,0 +1,59 @@
+package com.example.hw1;
+
+public class Cocktail {
+    public String strDrink;
+    public String strImageSource;
+    public String strInstructions;
+    public String[] strIngredients;
+    public String[] strMeasures;
+
+    public void setStrDrink(String strDrink){
+        this.strDrink = strDrink;
+    }
+    public void setStrImageSource(String strImageSource){this.strImageSource = strImageSource;}
+    public void setStrInstructions(String strInstructions){
+        this.strInstructions = strInstructions;
+    }
+    public void setStrIngredients(String[] strIngredients){
+        this.strIngredients = strIngredients;
+    }
+    public void setStrMeasures(String[] strMeasures){
+        this.strMeasures = strMeasures;
+    }
+
+
+
+//    public String strIngredient1;
+//    public String strIngredient2;
+//    public String strIngredient3;
+//    public String strIngredient4;
+//    public String strIngredient5;
+//    public String strIngredient6;
+//    public String strIngredient7;
+//    public String strIngredient8;
+//    public String strIngredient9;
+//    public String strIngredient10;
+//    public String strIngredient11;
+//    public String strIngredient12;
+//    public String strIngredient13;
+//    public String strIngredient14;
+//    public String strIngredient15;
+//
+//    public String strMeasure1;
+//    public String strMeasure2;
+//    public String strMeasure3;
+//    public String strMeasure4;
+//    public String strMeasure5;
+//    public String strMeasure6;
+//    public String strMeasure7;
+//    public String strMeasure8;
+//    public String strMeasure9;
+//    public String strMeasure10;
+//    public String strMeasure11;
+//    public String strMeasure12;
+//    public String strMeasure13;
+//    public String strMeasure14;
+//    public String strMeasure15;
+
+
+}

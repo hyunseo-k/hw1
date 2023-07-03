74186d900fef1fea4f895afe4159c6a36977aa6a
Jinhyeon Kwon <121283722+jinhyeonkwon@users.noreply.github.com>
2023-07-03 13:44:31 +0900

    tab3 : 검색 기능 recyclerView 제외 구현

--- app/src/main/java/com/example/hw1/Cocktail.java
+++ app/src/main/java/com/example/hw1/Cocktail.java
@@ -1,18 +1,53 @@
 package com.example.hw1;
 
+import java.util.ArrayList;
+
 public class Cocktail {
     public String strDrink;
     public String strImageSource;
     public String strInstructions;
-    public String[] strIngredients;
-    public String[] strMeasures;
+    public String strDrinkThumb;
+    //public String[] strIngredients;
+    //public String[] strMeasures;
+    public String strIngredient1;
+    public String strIngredient2;
+    public String strIngredient3;
+    public String strIngredient4;
+    public String strIngredient5;
+    public String strIngredient6;
+    public String strIngredient7;
+    public String strIngredient8;
+    public String strIngredient9;
+    public String strIngredient10;
+    public String strIngredient11;
+    public String strIngredient12;
+    public String strIngredient13;
+    public String strIngredient14;
+    public String strIngredient15;
 
-    public Cocktail(String strDrink, String strImageSource, String strInstructions, String[] strIngredients, String[] strMeasures) {
+    public String strMeasure1;
+    public String strMeasure2;
+    public String strMeasure3;
+    public String strMeasure4;
+    public String strMeasure5;
+    public String strMeasure6;
+    public String strMeasure7;
+    public String strMeasure8;
+    public String strMeasure9;
+    public String strMeasure10;
+    public String strMeasure11;
+    public String strMeasure12;
+    public String strMeasure13;
+    public String strMeasure14;
+    public String strMeasure15;
+
+    public Cocktail(String strDrink, String strImageSource, String strInstructions, String strDrinkThumb) {
         this.strDrink = strDrink;
         this.strImageSource = strImageSource;
         this.strInstructions = strInstructions;
-        this.strIngredients = strIngredients;
-        this.strMeasures = strMeasures;
+        this.strDrinkThumb = strDrinkThumb;
+        //this.strIngredients = strIngredients;
+        //this.strMeasures = strMeasures;
     }
 
     public String getStrDrink(){
@@ -21,14 +56,88 @@ public class Cocktail {
     public String getStrImageSource(){
         return strImageSource;
     }
+
+    public String getStrDrinkThumb(){
+        return strDrinkThumb;
+    }
+
     public String getStrInstructions(){
         return strInstructions;
     }
-    public String[] getStrIngredients(){
-        return strIngredients;
-    }
-    public String[] getStrMeasures(){
-        return strMeasures;
+//    public String[] getStrIngredients(){
+//        return strIngredients;
+//    }
+//    public String[] getStrMeasures(){
+//        return strMeasures;
+//    }
+
+    public ArrayList<String> getStrIngredients() {
+        ArrayList<String> result = new ArrayList<String>();
+
+        if (strIngredient1 == null) {
+            return result;
+        }
+        result.add(strIngredient1);
+        if (strIngredient2 == null) {
+            return result;
+        }
+        result.add(strIngredient2);
+        if (strIngredient3 == null) {
+            return result;
+        }
+        result.add(strIngredient3);
+        if (strIngredient4 == null) {
+            return result;
+        }
+        result.add(strIngredient4);
+        if (strIngredient5 == null) {
+            return result;
+        }
+        result.add(strIngredient5);
+        if (strIngredient6 == null) {
+            return result;
+        }
+        result.add(strIngredient6);
+        if (strIngredient7 == null) {
+            return result;
+        }
+        result.add(strIngredient7);
+        if (strIngredient8 == null) {
+            return result;
+        }
+        result.add(strIngredient8);
+        if (strIngredient9 == null) {
+            return result;
+        }
+        result.add(strIngredient9);
+        if (strIngredient10 == null) {
+            return result;
+        }
+        result.add(strIngredient10);
+        if (strIngredient11 == null) {
+            return result;
+        }
+        result.add(strIngredient11);
+        if (strIngredient12 == null) {
+            return result;
+        }
+        result.add(strIngredient12);
+        if (strIngredient13 == null) {
+            return result;
+        }
+        result.add(strIngredient13);
+        if (strIngredient14 == null) {
+            return result;
+        }
+        result.add(strIngredient14);
+        if (strIngredient15 == null) {
+            return result;
+        }
+        result.add(strIngredient15);
+
+        return result;
+
+
     }
 
     public void setStrDrink(String strDrink){
@@ -38,46 +147,16 @@ public class Cocktail {
     public void setStrInstructions(String strInstructions){
         this.strInstructions = strInstructions;
     }
-    public void setStrIngredients(String[] strIngredients){
-        this.strIngredients = strIngredients;
-    }
-    public void setStrMeasures(String[] strMeasures){
-        this.strMeasures = strMeasures;
-    }
+//    public void setStrIngredients(String[] strIngredients){
+//        this.strIngredients = strIngredients;
+//    }
+//    public void setStrMeasures(String[] strMeasures){
+//        this.strMeasures = strMeasures;
+//    }
 
 
 
-//    public String strIngredient1;
-//    public String strIngredient2;
-//    public String strIngredient3;
-//    public String strIngredient4;
-//    public String strIngredient5;
-//    public String strIngredient6;
-//    public String strIngredient7;
-//    public String strIngredient8;
-//    public String strIngredient9;
-//    public String strIngredient10;
-//    public String strIngredient11;
-//    public String strIngredient12;
-//    public String strIngredient13;
-//    public String strIngredient14;
-//    public String strIngredient15;
-//
-//    public String strMeasure1;
-//    public String strMeasure2;
-//    public String strMeasure3;
-//    public String strMeasure4;
-//    public String strMeasure5;
-//    public String strMeasure6;
-//    public String strMeasure7;
-//    public String strMeasure8;
-//    public String strMeasure9;
-//    public String strMeasure10;
-//    public String strMeasure11;
-//    public String strMeasure12;
-//    public String strMeasure13;
-//    public String strMeasure14;
-//    public String strMeasure15;
+
 
 
 }
--- app/src/main/java/com/example/hw1/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/hw1/ui/notifications/NotificationsFragment.java
@@ -9,14 +9,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.hw1.Cocktail;
 import com.example.hw1.R;
 import com.example.hw1.databinding.FragmentNotificationsBinding;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -67,12 +75,87 @@ public class NotificationsFragment extends Fragment {
                     public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                         final String myResponse = response.body().string();
 
-                        if (getActivity() != null) {
+                        JSONArray drinks;
+                        JSONObject jsonObject = null;
+                        Gson gson = new GsonBuilder().create();
+                        JSONObject firstDrink = null;
+
+                        try {
+                            jsonObject = new JSONObject(myResponse);
+                        } catch (JSONException e){
+                            Log.d("hello", "jsonObject 얻기 실패");
+                            throw new RuntimeException(e);
+                        }
+
+//                        if (!jsonObject.has("drinks")) {
+//                            Toast noResultToast = Toast.makeText(getContext(), "검색 결과가 없습니다.", Toast.LENGTH_SHORT);
+//                            noResultToast.show();
+//                            Log.d("hello", "검색 결과가 없어서 return합니다~");
+//                            return;
+//                        }
+
+                        try {
+                            drinks = jsonObject.getJSONArray("drinks");
+                        } catch (JSONException e){
                             getActivity().runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
                                     // 여기에 코드를 입력하세요
-                                    Log.d("hello", myResponse);
+                                    Toast noResultToast = Toast.makeText(getContext(), "검색 결과가 없습니다.", Toast.LENGTH_SHORT);
+                                    noResultToast.show();
+                                    Log.d("hello", "검색 결과가 없어서 return합니다~");
+                                }
+                            });
+
+                            return;
+                        }
+
+                        try {
+                            firstDrink = drinks.getJSONObject(0);
+                        } catch (JSONException e) {
+                            Log.d("hello", "firstDrink");
+                            throw new RuntimeException(e);
+                        }
+
+
+                        if (getActivity() != null) {
+                            JSONObject finalFirstDrink = firstDrink;
+                            getActivity().runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    // 여기에 코드를 입력하세요
+                                    Log.d("hello", String.valueOf(finalFirstDrink));
+
+                                    Cocktail resultCocktail = gson.fromJson(String.valueOf(finalFirstDrink), Cocktail.class);
+
+                                    // cocktailTitle
+                                    cocktailTitle.setText(resultCocktail.getStrDrink());
+                                    cocktailTitle.setVisibility(View.VISIBLE);
+
+                                    // cocktailImage
+                                    cocktailImage.setVisibility(View.VISIBLE);
+                                    if (resultCocktail.getStrDrinkThumb() == null) {
+                                        Glide.with(getContext())
+                                                .load(R.drawable.cocktails)
+                                                .into(cocktailImage);
+                                    } else {
+                                        Glide.with(getContext())
+                                                .load(resultCocktail.getStrDrinkThumb())
+                                                .into(cocktailImage);
+                                    }
+
+                                    // cocktailMethod
+                                    cocktailMethod.setVisibility(View.VISIBLE);
+                                    cocktailMethod.setText(resultCocktail.getStrInstructions());
+
+                                    // cocktailIngreds
+                                    if (resultCocktail.getStrIngredients().size() != 0) {
+                                        cocktailIngreds.setVisibility(View.VISIBLE);
+                                        Toast ingredsToast = Toast.makeText(getContext(), resultCocktail.getStrIngredients().toString(), Toast.LENGTH_SHORT);
+                                        ingredsToast.show();
+                                    }
+
+
                                 }
                             });
                         }

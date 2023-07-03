5b5a1197ce0b12f4009d1b49c51df49e07d6d50d
Jinhyeon Kwon <121283722+jinhyeonkwon@users.noreply.github.com>
2023-07-03 16:39:11 +0900

    gallary popup done

--- app/src/main/java/com/example/hw1/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/hw1/ui/dashboard/DashboardFragment.java
@@ -1,14 +1,24 @@
 package com.example.hw1.ui.dashboard;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +31,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 
@@ -30,6 +41,17 @@ public class DashboardFragment extends Fragment {
     private ListView listView;
     private MyGallaryAdapter myGallaryAdapter;
     private ArrayList<MyData> dataList;
+
+    ActivityResultLauncher<Intent> startActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Log.d("popup", "MainActivity로 돌아왔다.");
+                    }
+                }
+            });
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -64,12 +86,30 @@ public class DashboardFragment extends Fragment {
         myGallaryAdapter = new MyGallaryAdapter(this.getContext(), dataList);
         listView.setAdapter(myGallaryAdapter);
 
-        // 임시 코드 : 클릭하면 글자 띄우기
+        // listView의 아이템 클릭 시 해당 사진을 확대하는 팝업을 띄운다.
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast toast = Toast.makeText(getActivity(), "클릭되었습니다", Toast.LENGTH_SHORT);
                 toast.show();
+                
+                // 팝업 액티비티 호출하기
+
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                Bitmap bitmap = ((BitmapDrawable) ((ImageView) view.findViewById(R.id.gallary_img)).getDrawable()).getBitmap();
+                float scale = (float) (1024/(float)bitmap.getWidth());
+                int image_w = (int) (bitmap.getWidth() * scale);
+                int image_h = (int) (bitmap.getHeight() * scale);
+                Bitmap resize = Bitmap.createScaledBitmap(bitmap, image_w, image_h, true);
+                resize.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                Intent intent = new Intent(getContext(), GallaryPopup.class);
+                intent.putExtra("imgName", ((TextView) view.findViewById(R.id.gallary_text)).getText()); // text를 넘긴다.
+                intent.putExtra("imgView", byteArray); // 이미지를 넘긴다.
+                startActivityResult.launch(intent);
+
+                
             }
         });
 
---
+++ app/src/main/java/com/example/hw1/ui/dashboard/GallaryPopup.java
@@ -0,0 +1,53 @@
+package com.example.hw1.ui.dashboard;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.example.hw1.R;
+
+public class GallaryPopup extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 타이틀바 없애기
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.gallary_popup_activity);
+
+        // View들 가져오기
+        TextView imgName = (TextView) findViewById(R.id.imgName);
+        ImageView imgView = (ImageView) findViewById(R.id.imgView);
+        Button closeButton = (Button) findViewById(R.id.closeButton);
+
+        // main Activity에서 데이터 가져오기
+        Intent intent = getIntent();
+
+        imgName.setText(intent.getStringExtra("imgName"));
+        int imgId = intent.getIntExtra("imgView", R.drawable.person);
+        byte[] byteArray = getIntent().getByteArrayExtra("imgView");
+        Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        imgView.setImageBitmap(bitmap);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backMainActivity();
+            }
+        });
+    }
+
+    private void backMainActivity() {
+        setResult(Activity.RESULT_OK);
+        finish();
+    }
+}

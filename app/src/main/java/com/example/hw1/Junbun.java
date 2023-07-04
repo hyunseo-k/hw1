d331fa1067fbde1cd9762c1fe08ff8cf56bcab01
Jinhyeon Kwon <121283722+jinhyeonkwon@users.noreply.github.com>
2023-07-04 23:22:37 +0900

    search/add/modify bug fixed🎉

--- app/src/main/java/com/example/hw1/Junbun.java
+++ app/src/main/java/com/example/hw1/Junbun.java
@@ -6,11 +6,13 @@ public class Junbun implements Serializable {
     public String name;
     public String number;
     public String address;
+    public int id;
 
-    public Junbun(String name, String number, String address) {
+    public Junbun(String name, String number, String address, int id) {
         this.name = name;
         this.number = number;
         this.address = address;
+        this.id = id;
     }
 
 
@@ -23,6 +25,7 @@ public class Junbun implements Serializable {
     public String getAddress(){
         return address;
     }
+    public int getId() { return id; }
     public void setName(String name){
         this.name = name;
     }
@@ -32,5 +35,5 @@ public class Junbun implements Serializable {
     public void setAddress(String address){
         this.address = address;
     }
-
+    public void setId(int id) { this.id = id; }
 }
